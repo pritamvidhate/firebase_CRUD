165ee2e1f38323de32f080dcd4394c2a4e305156
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2021-10-16 11:07:01 +0530

    working and learning firebase concept

---
+++ src/firebaseConfg.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDAvGWnOogCC8v6Di0b3oFDAYNlcqp8VSU',
+  authDomain: 'fir-proj-303c3.firebaseapp.com',
+  projectId: 'fir-proj-303c3',
+  storageBucket: 'fir-proj-303c3.appspot.com',
+  messagingSenderId: '173896251449',
+  appId: '1:173896251449:web:117bb0849184b8847475e8',
+  measurementId: 'G-S69H0L67QP',
+};
+
+const app = initializeApp(firebaseConfig);
