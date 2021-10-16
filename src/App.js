a93dd1ea80e3c7f916ae9b21f091db8ff7395dad
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2021-10-16 11:46:31 +0530

    display users from firebase database on frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
+import { db } from './firebaseConfg';
+import { collection, getDocs } from 'firebase/firestore';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, 'users');
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
   return (
     <div className='App'>
-      <h1>hello firebase</h1>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/firebaseConfg.js
+++ src/firebaseConfg.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-
+import { getFirestore } from '@firebase/firestore';
 const firebaseConfig = {
   apiKey: 'AIzaSyDAvGWnOogCC8v6Di0b3oFDAYNlcqp8VSU',
   authDomain: 'fir-proj-303c3.firebaseapp.com',
@@ -11,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
